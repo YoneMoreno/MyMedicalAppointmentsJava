9ad62a177ed5c66f9e03e938c4dd65fbc786309f
Yone <yone.moreno101@alu.ulpgc.es>
2019-09-13 09:54:21 +0100

    Added Constructor to Doctor class

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,14 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Construyendo el objeto doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre asignado es: " + name);
+    }
+
     public void showName(){
         System.out.println(name);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Ana");
         myDoctor.name = "Ana";
         myDoctor.showName();
     }
