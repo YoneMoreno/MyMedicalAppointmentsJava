5b5a781a19fc34d078f991454c6a52150ada643e
Yone <yone.moreno101@alu.ulpgc.es>
2019-09-13 11:12:30 +0100

    Added Patient class

---
+++ src/Patient.java
@@ -0,0 +1,11 @@
+public class Patient {
+
+    String name, email, address, phoneNumber, birthday, blood;
+    double weight, height;
+
+    Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+}
